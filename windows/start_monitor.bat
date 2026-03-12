69b38a159482f03df5bc29d584297b7abb3b4fac
Andreas Wendorf <akustikrausch@gmail.com>
2026-03-12 15:52:59 +0100

    Rename repo to wt32-hw-monitor, update paths

--- windows/start_monitor.bat
+++ windows/start_monitor.bat
@@ -1,4 +1,4 @@
 @echo off
-cd /d C:\Users\andre\Downloads\ESP32\pc-monitor\windows
+cd /d C:\Users\andre\Downloads\ESP32\wt32-hw-monitor\windows
 C:\Users\andre\AppData\Local\Programs\Python\Python314-32\python.exe pc_monitor.py
 pause
